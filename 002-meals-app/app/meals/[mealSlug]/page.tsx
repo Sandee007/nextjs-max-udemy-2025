3ca8cfa27d5ac36e03fd12ecac37a83968602aa4
Sandee007 <sandeepaalahakoon@gmail.com>
2025-01-14 01:38:55 +0530

    dynamic metadata

--- 002-meals-app/app/meals/[mealSlug]/page.tsx
+++ 002-meals-app/app/meals/[mealSlug]/page.tsx
@@ -4,13 +4,35 @@ import { getMeal } from "@/lib/meals";
 import { Meal } from "@/components/meals-grid/meal-item/meal-item";
 import { notFound } from "next/navigation";
 
-export default async function MealDetailsPage({ params }: { params: Promise<{ mealSlug: string }> }) {
+interface Props {
+  params: Promise<{ mealSlug: string }>;
+}
+
+/** NOTE
+ * ! this is also a RESERVED function by nextjs
+ *
+ * * it receives the same props as the component
+ */
+export async function generateMetadata({ params }: Props) {
+  const meal = await getMeal((await params).mealSlug);
+
+  if(!meal){
+    notFound()
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
+export default async function MealDetailsPage({ params }: Props) {
   const promisedParams = await params;
   const meal: Meal = await getMeal(promisedParams?.mealSlug);
 
   // ! display nearest not-found error page - provided by nextjs
-  if(!meal){
-    notFound()
+  if (!meal) {
+    notFound();
   }
 
   // * convert newlines into linebreaks
--- 002-meals-app/app/meals/page.tsx
+++ 002-meals-app/app/meals/page.tsx
@@ -3,8 +3,14 @@ import styles from "./page.module.css";
 import MealsGrid from "@/components/meals-grid/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
+import { Metadata } from "next";
 
-// this will also be treated as a different server compoennt by nextjs
+export const metadata: Metadata = {
+  title: "Browse Meals",
+  description: "lorem ipsum",
+};
+
+// ! this will also be treated as a different server compoennt by nextjs
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
