34ed77629a1af12597f5e02aa68d212cd2bdde32
Sandee007 <sandeepaalahakoon@gmail.com>
2025-01-15 00:56:23 +0530

    server-side-data-fetching

--- 003-news-app/app/(route-group-content)/news/page.tsx
+++ 003-news-app/app/(route-group-content)/news/page.tsx
@@ -1,34 +1,12 @@
-"use client";
-
 import NewsList from "@/components/news-list/news-list";
-import { News } from "@/dummy-news";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [news, setNews] = useState<Array<News>>([]);
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response?.ok) {
+    throw new Error("Failed to fetch news.");
+  }
 
-  useEffect(() => {
-    async function fetchNews() {
-      setLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response?.ok) {
-        setError("Failed to fecth news");
-      }
-
-      const news = await response.json();
-      setNews(news);
-      setLoading(false);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p> {error} </p>;
+  const news = await response.json();
 
   return (
     <>
