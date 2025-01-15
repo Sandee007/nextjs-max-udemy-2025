26174afc31fb682bb28b88b58c62f5125473152c
Sandee007 <sandeepaalahakoon@gmail.com>
2025-01-15 23:35:40 +0530

    asasas

---
+++ 004-post-app/app/actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+import { uploadImage } from "@/lib/cloudinary";
+
+export interface FormResponse_createPost {
+  errors: Array<string>;
+}
+
+export async function createPost(
+  prevFormState: FormResponse_createPost,
+  formData: FormData
+): Promise<FormResponse_createPost> {
+  const title = formData.get("title") as string;
+  const image = formData.get("image") as File;
+  const content = formData.get("content") as string;
+
+  const errors = [];
+
+  if (!title || title?.trim()?.length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content?.trim()?.length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image?.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors?.length > 0) return { errors };
+
+  try {
+    const imageUrl = await uploadImage(image);
+    storePost({
+      imageUrl,
+      title,
+      content,
+      userId: 1,
+    });
+  } catch (error: unknown) {
+    throw new Error(error as string);
+  }
+
+  redirect("/");
+}
